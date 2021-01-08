f0bb30f11fecbd6d115824dbffd4c4d885042874
pangjiawei <pangjiawei@magictavern.com>
2021-01-08 11:19:31 +0800

    增加二分查找的几个变体实现

--- src/main/java/com/pjw/java/search/Search.java
+++ src/main/java/com/pjw/java/search/Search.java
@@ -6,13 +6,16 @@ package com.pjw.java.search;
  */
 public class Search {
 
+    /**
+     * 二分查找
+     */
     public static int binarySearch(int[] array, int n, int value) {
         int left = 0;
         int right = n - 1;
 
         while (left <= right) {
-            // 当 left 和 right 很大时，可能会移除，所以可以改写为：int middle = low + (high - low) / 2
-            // 为追求性能，可以进一步改写为：int middle = low + ((high - low) >> 1)
+            // 当 left 和 right 很大时，可能会移除，所以可以改写为：int middle = left + (right - left) / 2
+            // 为追求性能，可以进一步改写为：int middle = left + ((right - left) >> 1)
             int middle = (left + right) / 2;
             if (value == array[middle]) {
                 return middle;
@@ -20,6 +23,102 @@ public class Search {
                 right = middle - 1;
             } else {
                 left = middle + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * 二分查找，返回元素第一个出现的位置
+     */
+    public static int binarySearchFirst(int[] array, int n, int value) {
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+            int middle = left + ((right - left) >> 1);
+            if (value < array[middle]) {
+                right = middle - 1;
+            } else if (value > array[middle]) {
+                left = middle + 1;
+            } else {
+                if (middle == 0 || array[middle - 1] != value) {
+                    return middle;
+                } else {
+                    right = middle - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * 二分查找，返回元素最后一个出现的位置
+     */
+    public static int binarySearchFinal(int[] array, int n, int value) {
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+            int middle = left + ((right - left) >> 1);
+            if (value < array[middle]) {
+                right = middle - 1;
+            } else if (value > array[middle]) {
+                left = middle + 1;
+            } else {
+                if (middle == n - 1 || array[middle + 1] != value) {
+                    return middle;
+                } else {
+                    left = middle + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * 二分查找，返回第一个大于等于指定元素的位置
+     */
+    public static int binarySearchFirstEqualOrGreater(int[] array, int n, int value) {
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+            int middle = left + ((right - left) >> 1);
+            if (value <= array[middle]) {
+                if (middle == 0 || array[middle - 1] < value) {
+                    return middle;
+                } else {
+                    right = middle - 1;
+                }
+            } else if (value > array[middle]) {
+                left = middle + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * 二分查找，返回最后一个小于等于指定元素的位置
+     */
+    public static int binarySearchFinalEqualOrLess(int[] array, int n, int value) {
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+            int middle = left + ((right - left) >> 1);
+            if (value < array[middle]) {
+                right = middle - 1;
+            } else if (value >= array[middle]) {
+                if (middle == n - 1 || array[middle + 1] > value) {
+                    return middle;
+                } else {
+                    left = middle + 1;
+                }
             }
         }
 
--- src/test/java/com/pjw/java/search/SearchTest.java
+++ src/test/java/com/pjw/java/search/SearchTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 public class SearchTest {
 
     private final int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    private final int[] array2 = {1, 2, 3, 4, 4, 4, 4, 8, 9, 10};
 
     @Test
     void binarySearch() {
@@ -17,5 +18,41 @@ public class SearchTest {
         int index2 = Search.binarySearch(array, array.length, 15);
         Assertions.assertEquals(index1, 4);
         Assertions.assertEquals(index2, -1);
+    }
+
+    @Test
+    void binarySearchFirst() {
+        int index1 = Search.binarySearch(array2, array2.length, 4);
+        int index2 = Search.binarySearchFirst(array2, array2.length, 4);
+        int index3 = Search.binarySearchFirst(array2, array2.length, 15);
+        Assertions.assertNotEquals(index1, 3);
+        Assertions.assertEquals(index2, 3);
+        Assertions.assertEquals(index3, -1);
+    }
+
+    @Test
+    void binarySearchFinal() {
+        int index1 = Search.binarySearch(array2, array2.length, 4);
+        int index2 = Search.binarySearchFinal(array2, array2.length, 4);
+        int index3 = Search.binarySearchFinal(array2, array2.length, 15);
+        Assertions.assertNotEquals(index1, 6);
+        Assertions.assertEquals(index2, 6);
+        Assertions.assertEquals(index3, -1);
+    }
+
+    @Test
+    void binarySearchFirstEqualOrGreater() {
+        int index1 = Search.binarySearchFirstEqualOrGreater(array2, array2.length,4);
+        int index2 = Search.binarySearchFirstEqualOrGreater(array2, array2.length,5);
+        Assertions.assertEquals(index1, 3);
+        Assertions.assertEquals(index2, 7);
+    }
+
+    @Test
+    void binarySearchFinalEqualOrLess() {
+        int index2 = Search.binarySearchFinalEqualOrLess(array2, array2.length, 4);
+        int index3 = Search.binarySearchFinalEqualOrLess(array2, array2.length, 5);
+        Assertions.assertEquals(index2, 6);
+        Assertions.assertEquals(index3, 6);
     }
 }
